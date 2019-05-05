72526aae3df62df05cfb18cf1fda4f17f3b41a0a
Felix Hamme <git@fj.hamme.net>
2019-05-05 18:44:31 +0200

    add braces to if statements for readability

--- src/main/java/de/dhbw/se/refactoring/Rental.java
+++ src/main/java/de/dhbw/se/refactoring/Rental.java
@@ -14,14 +14,18 @@ class Rental {
         switch (movie.getPriceCode()) {
             case Movie.REGULAR:
                 rentalPrice += 2;
-                if (daysRented > 2) rentalPrice += (daysRented - 2) * 1.5;
+                if (daysRented > 2) {
+                    rentalPrice += (daysRented - 2) * 1.5;
+                }
                 break;
             case Movie.NEW_RELEASE:
                 rentalPrice += daysRented * 3;
                 break;
             case Movie.CHILDRENS:
                 rentalPrice += 1.5;
-                if (daysRented > 3) rentalPrice += (daysRented - 3) * 1.5;
+                if (daysRented > 3) {
+                    rentalPrice += (daysRented - 3) * 1.5;
+                }
                 break;
         }
         return rentalPrice;
