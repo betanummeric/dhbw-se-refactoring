0e6e79ad8a4b03630de4cd5b90fb47766a775e35
Felix Hamme <git@fj.hamme.net>
2019-05-05 19:19:43 +0200

    use java-style array declaration

--- src/main/java/de/dhbw/se/refactoring/Program.java
+++ src/main/java/de/dhbw/se/refactoring/Program.java
@@ -13,7 +13,7 @@ public class Program {
      */
 
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         String result;
         System.out.println("Welcome to the Movie Store");
         Movie m1 = new Movie("movie1", Movie.NEW_RELEASE);
