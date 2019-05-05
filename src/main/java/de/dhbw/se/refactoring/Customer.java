bf0f6553595e0b8099020a2d1966cd491c09e8fa
Felix Hamme <git@fj.hamme.net>
2019-05-05 16:41:34 +0200

    remove syntactically redundant semicolons

--- src/main/java/de/dhbw/se/refactoring/Customer.java
+++ src/main/java/de/dhbw/se/refactoring/Customer.java
@@ -11,19 +11,13 @@ class Customer {
         name = newname;
     }
 
-    ;
-
     public void addRental(Rental arg) {
         rentals.addElement(arg);
     }
 
-    ;
-
     public String getName() {
         return name;
     }
-
-    ;
 
     public String statement() {
         double totalAmount = 0;
